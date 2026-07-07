4690cbc82341c0263f2c9d45200ecad6487c1625
WICResearch <ali.m.coleman@wv.gov>
2026-07-07 15:04:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -518,9 +518,10 @@ console.error("Error:", error);
 });
 
 renderPage();
+
 document.getElementById("beginSurveyBtn").addEventListener("click", () => {
 document.getElementById("introPage").style.display = "none";
 document.getElementById("surveyForm").style.display = "block";
-document.getElementById("surveyForm").style.display = "none";
 renderPage();
+window.scrollTo({ top: 0, behavior: "smooth" });
 });
